10403f7b599e4635d2773b05fc9ca83b22deb802
sidemarschimmelpfennig <sidemarschimmelpfennig@outlook.com>
2024-12-10 20:35:05 -0300

    update order for project frontend

---
+++ frontend/src/views/DashBoardAdmin/pages/JobListingPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="c">
+    <router-view />
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style>
+.c {
+  height: 100vh;
+  width: 100%;
+}
+</style>
+
---
+++ frontend/src/views/JobListingView/Components/JobListingComponent.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="w-full job">
+    <div
+      class="w-full px-10 my-4 py-6 bg-white rounded-lg shadow-md job-component"
+      v-for="(jobs, id) in joblisting"
+      :key="id"
+    >
+      <div class="flex justify-between items-center job-header">
+        <span class="font-light text-gray-600">{{ jobs.start_date }}</span>
+        <label
+          class="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded"
+          href="#"
+          for=""
+          >{{ jobs.department }}</label
+        >
+      </div>
+      <div class="mt-2 job-content">
+        <a
+          class="text-2xl text-gray-700 font-bold hover:text-gray-600"
+          href="#"
+          >{{ jobs.jobname }}</a
+        >
+        <p class="mt-2 text-gray-600">{{ jobs.description }}</p>
+      </div>
+      <div class="flex justify-between items-center mt-4">
+        <a class="readmore" href="#">Saiba mais</a>
+        <div>
+          <h1 class="text-gray-700 font-bold">Remoto</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      joblist: [],
+    };
+  },
+  props: {
+    joblisting: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {},
+  mounted() {
+    this.joblist = this.joblisting;
+  },
+};
+</script>
+<style lang="scss">
+@import "@/assets/scss/components/joblistingcomponent";
+</style>
