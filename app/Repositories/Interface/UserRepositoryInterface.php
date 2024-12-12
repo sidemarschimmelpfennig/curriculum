a3f99be5953cdbe063f422b22e08be7c2c100f7c
sidemarschimmelpfennig <sidemarschimmelpfennig@outlook.com>
2024-12-12 19:24:55 -0300

    update where lodi rsrs

--- app/Repositories/Interface/UserRepositoryInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Repositories\Interface;
-
-interface UserRepositoryInterface extends BaseInterface
-{
-    public function findAdmin(int $param);
-    public function create(array $data);
-}
