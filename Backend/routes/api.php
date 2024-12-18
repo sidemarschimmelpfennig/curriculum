eef0dc114661f0cf2d4478a6b20a85b01da38bbd
João Lodi <joaolodi@SGBR.intra>
2024-12-18 13:49:39 -0300

    first

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -3,47 +3,65 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\{
-    CurriculumController,
-    JobController,
+    UserController,
+    CandidateController,
     LoginController,
-    AdminController
+    JobController,
 
 };
 
-Route::get('/download', [CurriculumController::class, 'downloadFile']); // download
-
-Route::post('/create', [CurriculumController::class, 'create']); // criar ""perfil""
-
 Route::prefix('register')->group(function () {
     Route::get('/login', function (){ return view('login'); });
-    Route::get('/login_method', [LoginController::class, 'login']);
-    Route::get('/logout_method', [LoginController::class, 'logout']);
-
-});
-
-// Rotas candidatos
-Route::middleware('auth:sanctum')->group(function (){
-    Route::prefix('v1/candidates')->group(function (){
-        Route::get('/jobs', [JobController::class, 'getAll']);
-        Route::get('/all/job/department/{department}', [JobController::class, 'findByDepartment']);
-        Route::get('/all/job/category/{category}', [JobController::class, 'findByDepartmentCategories']);
-        Route::get('/all/job/status/{status}', [JobController::class, 'findByStatus']);
-        Route::post('/send', [CurriculumController::class, 'send']);
-        Route::post('/create', [CurriculumController::class, 'create']);
-
-    });
+    Route::get('/create-accont', function (){ return view('newAccont'); });
     
-    // Rotas Administrativas
-    Route::prefix('v1/admin')->group(function (){    
-        Route::get('/jobs', [JobController::class, 'getAll']);
-        Route::post('/add-job', [JobController::class, 'createJob']);
-        Route::post('/add-departament', [JobController::class, 'createDepartament']);
-        Route::post('/add-departament_category', [JobController::class, 'createDepartamentCategory']);
-        Route::post('/add-status', [JobController::class, 'createStatus']);
-        Route::get('/newJobVacancy', [AdminController::class, 'view']);
-        Route::get('/send-file', function () {return view('file');});
-        Route::post('/send', [CurriculumController::class, 'send']);
+    Route::get('/login_method', [LoginController::class, 'getData']);
+    Route::get('/logout_method', [LoginController::class, 'logout']);
+    Route::post('/create', [CandidateController::class, 'create']);
+
+});
+
+
+    // Rotas candidatos
+Route::prefix('v1')->group( function () {
+    Route::get('/jobs', [JobController::class, 'getAll']);
+    
+    Route::prefix('/candidates')->group(function (){
+        Route::post('/send', [CandidateController::class, 'send']);
+        Route::post('/create', [CandidateController::class, 'create']);
+        
+        Route::get('/job/department/{department}', [JobController::class, 'findByDepartment']);
+        Route::get('/job/category/{category}', [JobController::class, 'findByDepartmentCategories']);
+        Route::get('/job/status/{status}', [JobController::class, 'findByStatus']);
+        Route::post('/apply', [JobController::class, 'apply']);
 
     });
-});
+        
+
+    Route::middleware('auth:sanctum')->group(function (){ 
+        Route::prefix('/admin')->group(function (){    
+
+            // + importante
+            Route::get('/jobs', [JobController::class, 'getAll']);
+            Route::post('/create-job', [JobController::class, 'create']);
+            Route::put('/deleteDepartament/{id}', [JobController::class, 'deleteDepartament']);
+            Route::put('/deleteUser/{id}', [UserController::class, 'delete']);
+            Route::put('/deleteCandidate/{id}', [CandidateController::class, 'delete']);
+                
+            // +- importante
+            Route::post('/create-departament', [JobController::class, 'createDepartament']);
+            Route::post('/create-departament_category', [JobController::class, 'createDepartamentCategory']);
+            Route::post('/create-status', [JobController::class, 'createStatus']);
+
+            // - importante
+            Route::post('/create-skills', [JobController::class, 'createSkills']);
+            Route::post('/create-mobilities', [JobController::class, 'createMobilities']);
+            
+            Route::put('/update-status', [JobController::class, 'updateStatus']);
+            Route::post('/send', [CandidateController::class, 'send']);
+            Route::get('/newJobVacancy', [UserController::class, 'view']); // view
+            Route::get('/send-file', function () {return view('file');});// view
+        });
+    });
+}); 
+
     
